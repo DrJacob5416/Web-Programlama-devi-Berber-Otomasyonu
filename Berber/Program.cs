ed86e84016cd0a83cadccd34fdbbb996572bc90c
Yakup Turan <yakupturan01@gmail.com>
2024-12-17 20:36:47 +0300

    Program.cs ve appsettings.json düzenlendi

--- Berber/Program.cs
+++ Berber/Program.cs
@@ -1,8 +1,37 @@
+using Berber.Models.Database;
+using Berber.Models.Tables;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSession();
 
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(opt =>
+{
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.SignIn.RequireConfirmedAccount = false;
+})
+    .AddRoleManager<RoleManager<ApplicationRole>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    opt.Cookie.Name = "BerberWeb";
+    opt.ExpireTimeSpan = TimeSpan.FromDays(7);
+    opt.SlidingExpiration = true;
+    opt.LoginPath = "/SignIn/Index";
+    opt.LogoutPath = "/SignOut/Index";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,11 +44,17 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSession();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapControllerRoute(
+   name: "areas",
+   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
